3319b01ff0ffba5c738519911045545ea40b1c72
main <main@example.com>
2014-11-08 18:01:06 +0900

    起動スクリプトを作成。

---
+++ start.sh
@@ -0,0 +1,14 @@
+#bin/bash
+
+if [ -d "${HOME}/.rbenv" ]; then
+    echo "data directory exists."
+else
+    mkdir ${$PWD}/data
+fi
+
+docker stop gitbucket
+docker rm gitbucket
+docker stop gitbucket-storage
+docker rm gitbucket-storage
+docker run --name gitbucket-storage -d -v ${PWD}/data:/root/.gitbucket -t nabezokodaikon/ubuntu:storage echo Data-only container for gitbucket.
+docker run --name gitbucket -d -p 127.0.0.1:8080:8080 --volumes-from gitbucket-storage -t nabezokodaikon/ubuntu:gitbucket
