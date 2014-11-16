15e93368c04e878959573e712cd7de1535d8698f
main <main@example.com>
2014-11-16 22:11:24 +0900

    バックアップスクリプト修正。
    一時的なコンテナでバックアップを実行するようにした。

--- backup.sh
+++ backup.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-docker run --volumes-from gitbucket-storage -v $(pwd):/backup nabezokodaikon/ubuntu:storage tar cvf /backup/backup.tar /root/.gitbucket
+
+if [ -e "$(pwd)/backup/backup.tar" ]; then
+    echo "Backup file exists."
+    echo "run '$(pwd)/backup/backup.tar'"
+    exit 1
+else
+    echo "Backup file does not exists."
+fi
+
+if [ -e "$(pwd)/backup" ]; then
+    echo "Backup directory exists."
+else
+    echo "Backup directory not exists."
+    mkdir $(pwd)/backup
+fi
+
+backupId=$(docker run --volumes-from gitbucket-storage -v $(pwd):/backup nabezokodaikon/ubuntu:storage tar cvf /backup/backup.tar /root/.gitbucket)
+docker rm ${backupId}
+
+exit 0
