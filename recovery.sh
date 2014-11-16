739e3665d9dd209758971381ef86a1ccc347a0cf
main <main@example.com>
2014-11-16 21:58:04 +0900

    リカバリスクリプト記述。

--- recovery.sh
+++ recovery.sh
@@ -1,1 +1,15 @@
 #!bin/bash
+
+if [ -e "${PWD}/backup/backup.tar" ]; then
+    echo "Backup file exists."
+else
+    echo "Backup file does not exists."
+    exit 1
+fi
+
+deleteId=$(docker run -d --volumes-from gitbucket-storage -t nabezokodaikon/ubuntu:storage rm -rf /root/.gitbucket)
+docker rm ${deleteId}
+
+recoveryId=$(docker run -d --volumes-from gitbucket-storage -v $(pwd)/backup:/backup -t nabezokodaikon/ubuntu:storage tar xvf backup/backup.tar)
+docker rm ${recoveryId}
+
